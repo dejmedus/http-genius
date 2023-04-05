082298e38df82e38e9633ccb4c7ad7c63d5bbb7d
dejmedus <email96123@gmail.com>
2023-04-04 18:44:20 -0600

    add answer checking

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 import { createQuestion } from './createQuestion';
 
-
-
 const typeSelect = document.getElementById('typeSelect');
 const imgEl = document.getElementById('imgEl');
 const qEl = document.getElementById('qEl');
@@ -9,28 +7,38 @@ const ansEl = document.getElementById('ansEl');
 const next = document.getElementById('next');
 
 
-
 // http status image type
 let type = 'dog'
 typeSelect.addEventListener('change', (e) => {
     type = (e.target.value).toLowerCase();
-    console.log(type)
 })
 
-console.log(typeSelect, imgEl, qEl, ansEl, next)
-
-
-next.addEventListener('click', async () => {
-
+// display the next question
+next.addEventListener('click', () => {
+    next.classList.add('hidden');
     const q = createQuestion()
-    imgEl.innerHTML = `<img src="${`https://http.${type}/${q.statusNum}.jpg`}" alt="${q.status}"/>`
+    imgEl.innerHTML = `<img class="hidden" src="${`https://http.${type}/${q.statusNum}.jpg`}" alt="${q.status}"/>`
 
-    qEl.innerHTML = `<h2>${q.statusNum}</h2>`
+    qEl.innerText = `${q.statusNum}`
+    qEl.classList.remove('none')
+
 
     for (let i = 0; i < 4; i++) {
-        ansEl.innerHTML += `<li value="${q.status == q.options[i]}">${q.options[i]}</li>`
+        ansEl.innerHTML += `<button name="${q.options[i]}" value="${q.options[i] == q.status}">${q.options[i]}</button>`
     }
+
+    ansEl.addEventListener('click', (e) => {
+        if (e.target.type == 'submit') {
+
+            // qEl.innerText = e.target.value == 'true' ? `Correct!` : `Incorrect`
+            qEl.classList.add('none')
+
+            // show ans image
+            imgEl.firstChild.classList.add(e.target.value == 'true' ? `correct` : `incorrect`);
+
+            imgEl.firstChild.classList.remove('hidden');
+            ansEl.innerHTML = ''
+            next.classList.remove('hidden');
+        };
+    })
 })
-
-
-
